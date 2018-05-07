06ca9148f83c2a32f27270036e189b089a4c1655
Leonardo Gatica <lgatica@protonmail.com>
2018-05-07 17:06:58 -0300

    chore(project): first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# sequelize-paginate
+
+[![npm version](https://img.shields.io/npm/v/sequelize-paginate.svg?style=flat-square)](https://www.npmjs.com/package/sequelize-paginate)
+[![npm downloads](https://img.shields.io/npm/dm/sequelize-paginate.svg?style=flat-square)](https://www.npmjs.com/package/sequelize-paginate)
+[![devDependency Status](https://img.shields.io/david/dev/eclass/sequelize-paginate.svg?style=flat-square)](https://david-dm.org/eclass/sequelize-paginate#info=devDependencies)
+
+> Sequelize model plugin for add paginate method
+
+## Installation
+
+```bash
+npm i sequelize-paginate
+```
+
+## Use
+
+```js
+// model.js
+const sequelizePaginate = require('sequelize-paginate')
+
+module.exports = (sequelize, DataTypes) => {
+  const MyModel = sequelize.define(
+    'MyModel',
+    {
+      name: { type: DataTypes.STRING(255) }
+    }
+  )
+  sequelizePaginate.paginate(MyModel)
+  return MyModel
+}
+
+// controller.js
+const { Op } = db.sequelize
+const options = {
+  attributes: ['id', 'name'],
+  page: 1,
+  paginate: 25,
+  order: [['name', 'DESC']],
+  where: { name: { [Op.like]: `%elliot%` } }
+}
+const { docs, pages } = await db.MyModel.paginate(options)
+```
+
+## License
+
+[MIT](https://tldrlegal.com/license/mit-license)
---
+++ src/index.js
@@ -0,0 +1,52 @@
+'use strict'
+
+class SequelizePaginate {
+  /**
+   * Sequelize Model
+   * @external {Model} http://docs.sequelizejs.com/class/lib/model.js~Model.html
+   */
+  /**
+   * Method to append paginate method to Model
+   * @param {Model} Model Sequelize Model
+   * @param {paginateOptions} options
+   */
+  paginate (Model) {
+    /**
+     * The paginate options
+     * @typedef {Object} paginateOptions
+     * @property {Array} attributes Model attributes
+     * @property {Object} where Model where
+     * @property {Array} order Model order
+     * @property {Number} paginate Results per page
+     * @property {Number} page Number of page
+     */
+    /**
+     * The paginate result
+     * @typedef {Object} paginateResult
+     * @property {Array} docs Docs
+     * @property {Number} pages Number of page
+     */
+    /**
+     * pagination
+     * @param {paginateOptions} params Options to filter query
+     * @return {paginateResult} Total pages and docs
+     */
+    const pagination = async function (params) {
+      const options = {
+        attributes: params.attributes,
+        where: params.where
+      }
+      const total = await Model.count({ where: options.where })
+      const pages = Math.ceil(total / params.paginate)
+      options.limit = params.paginate
+      options.offset = params.paginate * (params.page - 1)
+      if (params.order) options.order = params.order
+      const docs = await Model.findAll(options)
+      return { docs, pages }
+    }
+    const instanceOrModel = Model.Instance || Model
+    instanceOrModel.paginate = pagination
+  }
+}
+
+module.exports = new SequelizePaginate()
